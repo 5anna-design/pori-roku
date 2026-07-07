f2e4a0e93722fd6548f000acb055cf1cf29a945b
5anna-design <0683176085anna@gmail.com>
2026-07-07 21:19:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function spring() {
+    document.body.style.backgroundColor="#b8f5a2";
+    document.getElementById("title").innerHTML="Весна";
+    document.getElementById("text").innerHTML="Весна - це пора року, коли природа оживає після зимового сну. Дні стають довшими, температура підвищується, і рослини починають цвісти. Це час, коли люди відчувають прилив енергії та радості.";
+    document.getElementById("image").src="https://novy.tv/wp-content/uploads/sites/96/2026/05/istock-520413072-1024x683.jpg";
+}
+
+function summer() {
+    document.body.style.backgroundColor="#dfff6d";
+    document.getElementById("title").innerHTML="Літо";
+    document.getElementById("text").innerHTML="Літо - це пора року, коли сонце світить яскраво, а температура піднімається до високих значень. Люди насолоджуються відпочинком на пляжі, купанням у морі та активним відпочинком на природі. Це час відпусток та веселощів.";
+    document.getElementById("image").src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3VtbWVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60";
+
+}
+
+function autumn() {
+    document.body.style.backgroundColor="hsl(49, 63%, 42%)";
+    document.getElementById("title").innerHTML="Осінь";
+    document.getElementById("text").innerHTML="Осінь - це пора року, коли листя на деревах змінює колір на жовтий, помаранчевий та червоний. Температура починає знижуватися, а дні стають коротшими. Це час збору врожаю та підготовки до зими.";
+    document.getElementById("image").src="https://pogliad.ua/wp-content/uploads/2025/03/osin.jpg";
+}
+
+function winter() {
+    document.body.style.backgroundColor="hsl(210, 100%, 71%)";
+    document.getElementById("title").innerHTML="Зима";
+    document.getElementById("text").innerHTML="Зима - це пора року, коли температура опускається нижче нуля, і випадає сніг. Люди насолоджуються зимовими видами спорту, такими як катання на лижах та сноуборді. Це час святкувань та теплих вечорів біля каміна.";
+    document.getElementById("image").src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2ludGVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60";
+}
